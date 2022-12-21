7b204766007f536b2dfe01963db5ad5fadf3a52b
ithj <j.ith@vehikl.com>
2022-12-21 16:52:40 -0500

    Add basic functionality

---
+++ app.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+
+const { App } = require('@slack/bolt');
+
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+    // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+    // you still need to listen on some port!
+    port: process.env.PORT || 3000
+  });
+  
+  // Listens to incoming messages that contain "hello"
+  app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say({
+      blocks: [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": `Hey there <@${message.user}>!`
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "Click Me"
+            },
+            "action_id": "button_click"
+          }
+        }
+      ],
+      text: `Hey there <@${message.user}>!`
+    });
+  });
+  
+  app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+  });  
+
+  (async () => {
+    // Start your app
+    await app.start();
+  
+    console.log('⚡️ Bolt app is running!');
+  })();
+  
