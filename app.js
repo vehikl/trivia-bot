39711b69c96578db946a03674cb071bf11062659
ithj <j.ith@vehikl.com>
2023-01-25 17:05:33 -0500

    Add firebase and update env example

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
 require('dotenv').config()
+const { initializeApp } = require('firebase/app');
+const { getFirestore, collection, getDocs } = require('firebase/firestore/lite');
 
 const { App } = require('@slack/bolt');
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
+async function getQuizzes(db) {
+    const quizzesCol = collection(db, 'quizzes');
+    const quizSnapshot = await getDocs(quizzesCol);
+    const quizList = quizSnapshot.docs.map(doc => doc.data());
+    return quizList;
+}
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
@@ -173,7 +195,8 @@ const app = new App({
     // Acknowledge the action
     await ack();
     await say(`<@${body.user.id}> clicked the button`);
-  });  
+    await say(`<@${body.user.id}> ${await getQuizzes(db)}`);
+  });
 
   (async () => {
     // Start your app
---
+++ types/quizzes.ts
@@ -0,0 +1,6 @@
+interface Quizzes {
+    name: string
+    available_at: Date
+    created_at: Date
+    updated_at: Date
+}
