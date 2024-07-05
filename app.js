bfcaafc89b384a9d25cff5373ee50c539f2a9057
ithj <j.ith@vehikl.com>
2024-07-05 16:40:23 -0400

    add submit question functionality

--- app.js
+++ app.js
@@ -1,150 +1,87 @@
 import slackApp from '@slack/bolt';
 import dotenv from 'dotenv';
+import {addTrivia, getAll} from './models/quiz/quiz.js';
+
 dotenv.config();
-import { getAll } from './models/quiz/quiz.js';
-const { App } = slackApp;
+
+const {App} = slackApp;
 
 const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    socketMode: true,
-    appToken: process.env.SLACK_APP_TOKEN,
-    // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
-    // you still need to listen on some port!
-    port: process.env.PORT || 3000
-  });
-  
-  // Listens to incoming messages that contain "hello"
-  app.message('hello', async ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    await say({
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Hello Friend :wave:"
-                }
-            },
-            {
-                "type": "input",
-                "element": {
-                    "type": "plain_text_input",
-                    "multiline": true,
-                    "action_id": "plain_text_input-action"
-                },
-                "label": {
-                    "type": "plain_text",
-                    "text": "Question #1",
-                    "emoji": true
-                }
-            },
-            {
-                "type": "input",
-                "element": {
-                    "type": "plain_text_input",
-                    "action_id": "plain_text_input-action"
-                },
-                "label": {
-                    "type": "plain_text",
-                    "text": "Answer to question #1",
-                    "emoji": true
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": {
-                            "type": "plain_text",
-                            "text": "Submit",
-                            "emoji": true
-                        },
-                        "action_id": "button_click",
-                        "value": "submit_trivia"
-                    }
-                ]
-            }
-        ]
-    });
-  });
-
-  app.action('button_click', async ({ body, ack, say }) => {
-    // Acknowledge the action
-    await ack();
-    await say(`<@${body.user.id}> clicked the button`);
-      console.log(body)
-    await say(`<@${body.user.id}> ${
-        await getAll()
-    }`);
-  });
-
-app.command('/trivia', async ({ ack, body, client, logger }) => {
-    // Acknowledge the command request
-    await ack();
-
-    try {
-        // Call views.open with the built-in client
-        const result = await client.views.open({
-            // Pass a valid trigger_id within 3 seconds of receiving it
-            trigger_id: body.trigger_id,
-            // View payload
-            view: {
-                type: 'modal',
-                // View identifier
-                callback_id: 'view_1',
-                title: {
-                    type: 'plain_text',
-                    text: 'Modal title'
-                },
-                blocks: [
-                    {
-                        type: 'section',
-                        text: {
-                            type: 'mrkdwn',
-                            text: 'Welcome to a modal with _blocks_'
-                        },
-                        accessory: {
-                            type: 'button',
-                            text: {
-                                type: 'plain_text',
-                                text: 'Click me!'
-                            },
-                            action_id: 'button_abc'
-                        }
-                    },
-                    {
-                        type: 'input',
-                        block_id: 'input_c',
-                        label: {
-                            type: 'plain_text',
-                            text: 'What are your hopes and dreams?'
-                        },
-                        element: {
-                            type: 'plain_text_input',
-                            action_id: 'dreamy_input',
-                            multiline: true
-                        }
-                    }
-                ],
-                submit: {
-                    type: 'plain_text',
-                    text: 'Submit'
-                }
-            }
-        });
-        logger.info(result);
-    }
-    catch (error) {
-        logger.error(error);
-    }
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  // Socket Mode doesn't listen on a port, but in case you want your app to respond to OAuth,
+  // you still need to listen on some port!
+  port: process.env.PORT || 3000,
 });
 
-  (async () => {
-    // Start your app
-    await app.start();
-  
-    console.log('⚡️ Bolt app is running!');
-  })();
-  
+app.view('view_1', async ({ ack, body, view, client, logger }) => {
+  await ack();
+
+  const question = view['state']['values']['input_question'].trivia_question.value;
+  const answer = view['state']['values']['input_answer'].dreamy_input.value;
+  const user = body['user']['id'];
+  logger(question);
+  logger(answer);
+  await addTrivia(question, answer);
+});
+
+app.command('/trivia', async ({ack, body, client, logger}) => {
+  await ack();
+
+  try {
+    const result = await client.views.open({
+      trigger_id: body.trigger_id,
+      view: {
+        type: 'modal',
+        callback_id: 'view_1',
+        title: {
+          type: 'plain_text',
+          text: 'Modal title',
+        },
+        blocks: [
+          {
+            type: 'input',
+            block_id: 'input_question',
+            label: {
+              type: 'plain_text',
+              text: 'Question',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'trivia_question',
+              multiline: true,
+            },
+          },
+          {
+            type: 'input',
+            block_id: 'input_answer',
+            label: {
+              type: 'plain_text',
+              text: 'What are your hopes and dreams?',
+            },
+            element: {
+              type: 'plain_text_input',
+              action_id: 'dreamy_input',
+              multiline: true,
+            },
+          },
+        ],
+        submit: {
+          type: 'plain_text',
+          text: 'Submit',
+        },
+      },
+    });
+  } catch (error) {
+    logger.error(error);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start();
+
+  console.log('⚡️ Bolt app is running!');
+})();
--- models/quiz/quiz.js
+++ models/quiz/quiz.js
@@ -1,8 +1,18 @@
-import { collection, getDocs } from 'firebase/firestore/lite';
+import { collection, getDocs, doc,  setDoc} from 'firebase/firestore/lite';
 import firebaseDatabase from '../../services/firebase/databaseConnection.js';
 
 export async function getAll() {
         const quizzesCol = collection(firebaseDatabase, 'quizzes');
         const quizSnapshot = await getDocs(quizzesCol);
         return quizSnapshot.docs.map(doc => doc.data());
+}
+
+export async function addTrivia(question, answer) {
+  // Add a new document in collection "cities"
+  const newDoc = await setDoc(doc(firebaseDatabase, "questions", "tacos"), {
+    question,
+    answer,
+  });
+
+  console.log('newDoc', newDoc);
 }
