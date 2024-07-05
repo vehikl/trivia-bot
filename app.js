8b89ba5d9f58467be388c63f35b99fb72a6ed41f
IthJ <johnith1111@hotmail.com>
2024-07-05 15:05:20 -0400

    Add a view to play with

--- app.js
+++ app.js
@@ -51,107 +51,6 @@ const app = new App({
                     "emoji": true
                 }
             },
-            // Commented out for faster manual testing
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "multiline": true,
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Question #2",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Answer to question #2",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "multiline": true,
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Question #3",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Answer to question #3",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "multiline": true,
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Question #4",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Answer to question #4",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "multiline": true,
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Question #5",
-            //         "emoji": true
-            //     }
-            // },
-            // {
-            //     "type": "input",
-            //     "element": {
-            //         "type": "plain_text_input",
-            //         "action_id": "plain_text_input-action"
-            //     },
-            //     "label": {
-            //         "type": "plain_text",
-            //         "text": "Answer to question #5",
-            //         "emoji": true
-            //     }
-            // },
             {
                 "type": "actions",
                 "elements": [
@@ -170,13 +69,77 @@ const app = new App({
         ]
     });
   });
-  
+
   app.action('button_click', async ({ body, ack, say }) => {
     // Acknowledge the action
     await ack();
     await say(`<@${body.user.id}> clicked the button`);
-    await say(`<@${body.user.id}> ${await getAll()}`);
+      console.log(body)
+    await say(`<@${body.user.id}> ${
+        await getAll()
+    }`);
   });
+
+app.command('/trivia', async ({ ack, body, client, logger }) => {
+    // Acknowledge the command request
+    await ack();
+
+    try {
+        // Call views.open with the built-in client
+        const result = await client.views.open({
+            // Pass a valid trigger_id within 3 seconds of receiving it
+            trigger_id: body.trigger_id,
+            // View payload
+            view: {
+                type: 'modal',
+                // View identifier
+                callback_id: 'view_1',
+                title: {
+                    type: 'plain_text',
+                    text: 'Modal title'
+                },
+                blocks: [
+                    {
+                        type: 'section',
+                        text: {
+                            type: 'mrkdwn',
+                            text: 'Welcome to a modal with _blocks_'
+                        },
+                        accessory: {
+                            type: 'button',
+                            text: {
+                                type: 'plain_text',
+                                text: 'Click me!'
+                            },
+                            action_id: 'button_abc'
+                        }
+                    },
+                    {
+                        type: 'input',
+                        block_id: 'input_c',
+                        label: {
+                            type: 'plain_text',
+                            text: 'What are your hopes and dreams?'
+                        },
+                        element: {
+                            type: 'plain_text_input',
+                            action_id: 'dreamy_input',
+                            multiline: true
+                        }
+                    }
+                ],
+                submit: {
+                    type: 'plain_text',
+                    text: 'Submit'
+                }
+            }
+        });
+        logger.info(result);
+    }
+    catch (error) {
+        logger.error(error);
+    }
+});
 
   (async () => {
     // Start your app
