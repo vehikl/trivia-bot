1aa1ad6a9bb60717de50fe8bdd740d4719a98c55
ithj <j.ith@vehikl.com>
2024-09-13 09:24:17 -0400

    add feature to get next and previous quizzes

--- models/quiz/quiz.js
+++ models/quiz/quiz.js
@@ -1,4 +1,4 @@
-import {collection, doc, getDocs, getDoc, setDoc} from 'firebase/firestore/lite';
+import {collection, doc, getDoc, getDocs, query, setDoc, where} from 'firebase/firestore/lite';
 import firebaseDatabase from '../../services/firebase/databaseConnection.js';
 
 export async function getAll() {
@@ -22,8 +22,47 @@ export async function getTrivia(topic) {
   if (quizSnapshot.exists()) {
     return quizSnapshot.data();
   } else {
-    console.log("No such document!");
+    console.log('No such document!');
   }
+}
+
+export async function getNextTrivia() {
+  const nextThursday = getNextThursday();
+  const startOfDay = new Date(nextThursday.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(nextThursday.setHours(23, 59, 59, 999));
+  let nextTrivia;
+
+  const triviaRef = collection(firebaseDatabase, 'quizzes');
+
+  const nextTriviaQuery = query(triviaRef, where('date', '<=', endOfDay), where('date', '>=', startOfDay));
+
+  const snapshot = await getDocs(nextTriviaQuery);
+
+  snapshot.forEach(doc => {
+    nextTrivia = doc.data();
+  });
+
+  return nextTrivia;
+}
+
+export async function getPreviousTrivia() {
+  const nextThursday = getNextThursday();
+  const startOfDay = new Date(nextThursday.setHours(0, 0, 0, 0));
+  const endOfDay = new Date(nextThursday.setHours(23, 59, 59, 999));
+  let previousTrivia;
+
+  const triviaRef = collection(firebaseDatabase, 'quizzes');
+
+  const nextTriviaQuery = query(triviaRef, where('date', '<=', startOfDay));
+
+  const snapshot = await getDocs(nextTriviaQuery);
+
+  snapshot.forEach(doc => {
+    previousTrivia = doc.data();
+    return previousTrivia;
+  });
+
+  return previousTrivia;
 }
 
 export async function store(quiz) {
@@ -34,4 +73,15 @@ export async function store(quiz) {
     console.error(e);
     return false;
   }
+}
+
+function getNextThursday() {
+  const today = new Date();
+  const dayOfWeek = today.getDay(); // Sunday = 0, Monday = 1, ..., Thursday = 4
+  const daysUntilThursday = (4 - dayOfWeek + 7) % 7; // Calculate days until next Thursday
+  const nextThursday = new Date(today);
+
+  nextThursday.setDate(today.getDate() + daysUntilThursday);
+
+  return nextThursday;
 }
