f53ec88dfe280c641423b6a93e433ac464aa9e4b
VehiklAurora <aurora@vehikl.com>
2025-01-14 17:03:32 -0500

    Add regenerate feature for trivia command

--- commands/trivia.js
+++ commands/trivia.js
@@ -1,197 +1,198 @@
 import OpenAI from 'openai';
-import {getAllTopics, store, getPreviousTrivia} from '../models/quiz/quiz.js';
+import { store } from '../models/quiz/quiz.js';
 
 const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY});
+let topic = '';
 
 export function triviaCommand(app) {
   app.command('/trivia', async ({ack, body, say}) => {
     await ack();
 
-    const topic = body.text;
+     await app.client.chat.postEphemeral({
+       channel: body.channel_id,
+       user: body.user_id,
+       text: 'Generating Trivia... :brain:'
+     })
 
-    const completion = await openai.chat.completions.create({
-      messages: [
-        {
-          role: 'system', content: 'You will be provided a topic. You will need to create trivia questions from this topic. Make sure that the message given to the User is string' +
-              ' format so I can JSON parse it. Please only provide 5 questions. \n' +
-              '\n' +
-              'Make sure each string has a property of questions with each object having a question, answers as an array with multiple choice of a,b,c, and d in their own objects' +
-              ' the right answer. An example of the string format would be: [{question: "question text", answers: ["a) Example answer 1","b) Example answer 2","c) Example' +
-              ' answer 3" "d) Example answer 4", correctAnswer: b]},' +
-              ' ...] This is the' +
-              ' topic:' +
-              ' ' + topic,
-        },
-      ],
-      model: 'gpt-4o',
-    });
-
-    const response = JSON.parse(completion.choices[0].message.content);
-
-    let questionBlocks = [];
-    response.forEach((item, index) => {
-      questionBlocks.push(
-          {
-            'type': 'section',
-            'text': {
-              'type': 'mrkdwn',
-              'text': `*Question ${index + 1}: ${item.question}*`,
-            },
-          },
-      );
-
-      item.answers.forEach((answer) => {
-        questionBlocks.push(
-            {
-              'type': 'section',
-              'text': {
-                'type': 'mrkdwn',
-                'text': answer,
-              },
-            },
-        );
-      });
-
-      questionBlocks.push(
-          {
-            'type': 'section',
-            'text': {
-              'type': 'mrkdwn',
-              'text': `*Answer: ${item.correctAnswer}*`,
-            },
-          },
-      );
-    });
-
-    app.action('submit', async ({ack}) => {
-      await ack();
-
-      const questions = response.map(item => ({
-        question: item.question,
-        options: item.answers,
-        correctAnswer: item.correctAnswer,
-      }));
-
-      const quiz = {
-        topic,
-        questions,
-      };
-      await store(quiz);
-    });
-
-    await say({
-      'text': 'Trivia Based on ' + body.text,
-      'blocks': [
-        {
-          'type': 'header',
-          'text': {
-            'type': 'plain_text',
-            'text': 'Hello Friend! Time for Trivia! :wave:',
-          },
-        },
-        ...questionBlocks,
-        {
-          'type': 'divider',
-        },
-        {
-          'type': 'context',
-          'elements': [
-            {
-              'type': 'mrkdwn',
-              'text': 'Make sure to review the answers and questions before submitting! 💡',
-            },
-          ],
-        },
-        {
-          'type': 'actions',
-          'elements': [
-            {
-              'type': 'button',
-              'text': {
-                'type': 'plain_text',
-                'text': 'Submit',
-                'emoji': true,
-              },
-              'value': 'submit_button',
-              'action_id': 'submit',
-            },
-          ],
-        },
-      ],
-    });
-  });
-
-  app.command('/all', async ({ack, say}) => {
-    await ack();
-
-    const quizTitles = await getAllTopics();
-
-    const botSays = quizTitles.join('\n');
-
-    await say({
-      'text': 'All trivia titles',
-      'blocks' : [
-        {
-          "type": "header",
-          "text": {
-            "type": "plain_text",
-            "text": "All Trivia topics:",
-            "emoji": true
-          }
-        },
-        {
-          "type": "section",
-          "text": {
-            "type": "plain_text",
-            "text": botSays,
-            "emoji": true
-          }
-        }
-      ]
-    })
-  });
-
-  app.command('/answers', async ({ack, say}) => {
-    await ack();
-
-    const trivia = await getPreviousTrivia();
-    const quizTitle = trivia.topic;
-
-     let questionBlocks = [];
-      trivia.questions.forEach((item, index) => {
-          questionBlocks.push(
-              {
-                'type': 'section',
-                'text': {
-                  'type': 'mrkdwn',
-                  'text': `*Question ${index + 1}: ${item.question}*`,
-                },
-              },
-          );
-        questionBlocks.push(
-            {
-              'type': 'section',
-              'text': {
-                'type': 'mrkdwn',
-                'text': `Answer: *${item.correctAnswer}*`,
-              },
-            },
-        );
-      })
-
-    await say({
-      'text': 'previous trivia title',
-      'blocks' : [
-        {
-          "type": "header",
-          "text": {
-            "type": "plain_text",
-            "text": quizTitle,//movies
-            "emoji": true
-          }
-        },
-          ...questionBlocks
-      ]
-    })
+    try {
+      await executeCommand(app, body, say);
+    } catch (error) {
+      console.log(error);
+      await executeCommand(app, body, say);
+    }
   });
 }
+
+const executeCommand = async (app, body, say) => {
+  topic = body.text;
+
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: 'system', content: 'You will be provided a topic. You will need to create trivia questions from this topic. Make sure that the message given to the User is string' +
+            ' format so I can JSON parse it. Please only provide 5 questions. \n' +
+            '\n' +
+            'Make sure each string has a property of questions with each object having a question, answers as an array with multiple choice of a,b,c, and d in their own objects' +
+            ' the right answer. An example of the string format would be: [{question: "question text", answers: ["a) Example answer 1","b) Example answer 2","c) Example' +
+            ' answer 3" "d) Example answer 4", correctAnswer: b]},' +
+            ' ...] This is the' +
+            ' topic:' +
+            ' ' + topic,
+      },
+    ],
+    model: 'gpt-4o',
+  });
+
+  const response = JSON.parse(completion.choices[0].message.content);
+
+  // const response = exampleTriviaResponse;
+
+  let questionBlocks = [];
+
+  response.forEach((item, index) => {
+    questionBlocks.push(
+        {
+          'type': 'section',
+          'text': {
+            'type': 'mrkdwn',
+            'text': `*Question ${index + 1}: ${item.question}*`,
+          },
+        },
+    );
+
+    item.answers.forEach((answer) => {
+      questionBlocks.push(
+          {
+            'type': 'section',
+            'text': {
+              'type': 'mrkdwn',
+              'text': item.correctAnswer === answer[0] ? `*${answer}* :white_check_mark:` : `${answer}`,
+            },
+          },
+      );
+    });
+  });
+
+  app.action('submit', async ({ack}) => {
+    await ack();
+
+    const questions = response.map(item => ({
+      question: item.question,
+      options: item.answers,
+      correctAnswer: item.correctAnswer,
+    }));
+
+    const quiz = {
+      topic,
+      questions,
+    };
+    await store(quiz);
+  });
+
+  app.action('regenerate', async ({ack}) => {
+    await ack();
+
+    await app.client.chat.postEphemeral({
+      channel: body.channel_id,
+      user: body.user_id,
+      text: 'Regenerating Trivia... :brain:'
+    })
+
+    await executeCommand(app, body, say);
+  });
+
+  await say({
+    'text': 'Trivia Based on ' + body.text,
+    'blocks': [
+      {
+        'type': 'header',
+        'text': {
+          'type': 'plain_text',
+          'text': 'Hello Friend! It\'s time for Trivia! :bulb: :brain:',
+        },
+      },
+      ...questionBlocks,
+      {
+        'type': 'divider',
+      },
+      {
+        'type': 'context',
+        'elements': [
+          {
+            'type': 'mrkdwn',
+            'text': 'Make sure to review the answers and questions before submitting! 💡',
+          },
+        ],
+      },
+      {
+        "type": "section",
+        "block_id": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Pick a date for when the Trivia will be for."
+        },
+        "accessory": {
+          "type": "datepicker",
+          "action_id": "datepicker",
+          "placeholder": {
+            "type": "plain_text",
+            "text": "Select a Date"
+          }
+        }
+      },
+      {
+        'type': 'actions',
+        'elements': [
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'text': 'Submit',
+              'emoji': true,
+            },
+            'value': 'submit_button',
+            'action_id': 'submit',
+          },
+          {
+            'type': 'button',
+            'text': {
+              'type': 'plain_text',
+              'text': 'Regenerate',
+              'emoji': true,
+            },
+            'value': 'regenerate_button',
+            'action_id': 'regenerate',
+          },
+        ],
+      },
+    ],
+  });
+}
+
+const exampleTriviaResponse = [
+  {
+    question: "Which fruit is known as the 'King of Fruits'?",
+    answers: [ 'a) Apple', 'b) Mango', 'c) Banana', 'd) Pineapple' ],
+    correctAnswer: 'b'
+  },
+  {
+    question: 'Which fruit has the highest vitamin C content per 100g?',
+    answers: [ 'a) Orange', 'b) Kiwi', 'c) Strawberry', 'd) Guava' ],
+    correctAnswer: 'd'
+  },
+  {
+    question: 'What is the main ingredient in traditional guacamole?',
+    answers: [ 'a) Avocado', 'b) Tomato', 'c) Bell pepper', 'd) Olive' ],
+    correctAnswer: 'a'
+  },
+  {
+    question: 'Which fruit is botanically classified as a berry?',
+    answers: [ 'a) Raspberry', 'b) Strawberry', 'c) Blueberry', 'd) Banana' ],
+    correctAnswer: 'd'
+  },
+  {
+    question: "Which fruit is known for having a 'star-shaped' cross section when cut?",
+    answers: [ 'a) Papaya', 'b) Starfruit', 'c) Kiwi', 'd) Dragonfruit' ],
+    correctAnswer: 'b'
+  }
+];
